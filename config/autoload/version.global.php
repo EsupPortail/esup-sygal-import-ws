8e3ebf1dbaa52478aa39810928ecec288e4ecfe7
Bertrand Gauthier <bertrand.gauthier@unicaen.fr>
2019-04-09 15:16:01 +0200

    Mise à jour de la date de version

--- config/autoload/version.global.php
+++ config/autoload/version.global.php
@@ -3,8 +3,8 @@ return [
     'unicaen-app' => [
         'app_infos' => [
             'version' => '1.2.5',
-            'date' => '01/04/2019',
+            'date' => '09/04/2019',
         ],
     ],
-    'comment' => 'Fichier généré le 01/04/2019 à 09:29:18 avec /home/gauthierb/workspace/sygal-import-ws/bump-version',
+    'comment' => 'Fichier généré le 09/04/2019 à 15:15:41 avec /home/gauthierb/workspace/sygal-import-ws/bump-version',
 ];
