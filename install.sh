6d9b603935a0ea0d30124f2d3923ea40ce2cf581
Bertrand GAUTHIER <bertrand.gauthier@unicaen.fr>
2020-12-09 14:28:43 +0100

    Déplacement des commandes d'install.sh vers composer.json

--- install.sh
+++ install.sh
@@ -10,17 +10,3 @@ composer install --no-dev --no-suggest --optimize-autoloader
 
 # Désactivation de l'interface d'admin Apigility
 composer development-disable
-
-# Répertoires de travail de Doctrine
-mkdir -p data/cache                   && chmod -R 777 data/cache
-mkdir -p data/DoctrineModule/cache    && chmod -R 777 data/DoctrineModule/cache
-mkdir -p data/DoctrineORMModule/Proxy && chmod -R 777 data/DoctrineORMModule/Proxy
-rm -rf data/cache/*
-rm -rf data/DoctrineModule/cache/*
-rm -rf data/DoctrineORMModule/Proxy/*
-
-# Commandes Doctrine
-vendor/bin/doctrine-module orm:clear-cache:query
-vendor/bin/doctrine-module orm:clear-cache:metadata
-vendor/bin/doctrine-module orm:clear-cache:result
-vendor/bin/doctrine-module orm:generate-proxies
