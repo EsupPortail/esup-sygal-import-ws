6ef2664b05ea5887d04900737eee33d3e6db055f
Bertrand GAUTHIER <bertrand.gauthier@unicaen.fr>
2023-03-01 08:57:49 +0100

    Corrections des releases-notes : il faut aussi mettre à jour la vue V_SYGAL_VARIABLE

--- doc/release-notes/2.3.0/apogee.sql
+++ doc/release-notes/2.3.0/apogee.sql
@@ -2,6 +2,23 @@
 -- Apogée.
 --
 
+create or replace view V_SYGAL_VARIABLE as
+select
+    'apogee'            as source_id,       -- Id de la source
+    cod_vap             as id,
+    cod_vap,
+    lib_vap,
+    par_vap,
+    to_date('2017-01-01', 'YYYY-MM-DD') as DATE_DEB_VALIDITE,
+    to_date('9999-12-31', 'YYYY-MM-DD') as DATE_FIN_VALIDITE
+from variable_appli
+where cod_vap in (
+    'ETB_LIB',
+    'ETB_ART_ETB_LIB',
+    'ETB_LIB_TIT_RESP',
+    'ETB_LIB_NOM_RESP'
+);
+
 create or replace view V_SYGAL_VARIABLE_V2 as
 select
     cod_vap as source_code, -- Id unique
--- doc/release-notes/2.3.0/physalis.sql
+++ doc/release-notes/2.3.0/physalis.sql
@@ -2,7 +2,7 @@
 -- Physalis.
 --
 
--- Dans la vue V_SYGAL_VARIABLE_V2, supprimez les 3 lignes correspondant aux SOURCE_CODE suivants :
+-- Dans les vues V_SYGAL_VARIABLE et V_SYGAL_VARIABLE_V2, supprimez les 3 lignes correspondant aux SOURCE_CODE suivants :
 --   - 'EMAIL_ASSISTANCE'
 --   - 'EMAIL_BU'
 --   - 'EMAIL_BDD'
