a411b2f171a4e2ca70b97a2b585ae385646eeb2a
Bertrand GAUTHIER <bertrand.gauthier@unicaen.fr>
2024-04-19 16:04:30 +0200

    Correction install.sh

--- install.sh
+++ install.sh
@@ -13,7 +13,14 @@ CURDIR=$(cd `dirname $0` && pwd)
 cd ${CURDIR}
 
 # Composer install
-composer install --no-interaction --no-suggest --prefer-dist --optimize-autoloader
+composer install --no-interaction --prefer-dist --optimize-autoloader
+
+# Création ou vidange des répertoires de cache
+mkdir -p data/cache && chmod 777 data/cache && rm -rf data/cache/*
+mkdir -p data/DoctrineModule/cache && chmod 777 data/DoctrineModule/cache && rm -rf data/DoctrineModule/cache/*
+mkdir -p data/DoctrineORMModule/Proxy && chmod 777 data/DoctrineORMModule/Proxy && rm -rf data/DoctrineORMModule/Proxy/*
+
+vendor/bin/laminas-development-mode enable
 
 vendor/bin/doctrine-module orm:generate-proxies
 vendor/bin/doctrine-module orm:clear-cache:query
@@ -21,7 +28,3 @@ vendor/bin/doctrine-module orm:clear-cache:metadata
 vendor/bin/doctrine-module orm:clear-cache:result
 
 vendor/bin/laminas-development-mode disable
-
-# Création ou vidange des répertoires de cache
-mkdir -p data/cache && chmod -R 777 data/cache && rm -rf data/cache/*
-mkdir -p data/DoctrineModule/cache && chmod -R 777 data/DoctrineModule/cache && rm -rf data/DoctrineModule/cache/*
